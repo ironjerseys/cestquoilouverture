84c66efea27593b74eacee19f4ca7c32a62f7e55
Joris Reynes <jo.r6349@gmail.com>
2024-12-09 21:28:30 +0100

    update settings

--- cestquoilouverture/settings.py
+++ cestquoilouverture/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-_-^#4p$lb1)$s2z0gec++03^c$c$!l#u@gswu)$zu$*f78brgy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'cestquoilouverture-dmethmd7dtfvbyb3.francecentral-01.azurewebsites.net',
+]
+
 
 
 # Application definition
