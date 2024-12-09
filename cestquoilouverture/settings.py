e9679f68d4b035f00bd7a4e704bdcedeaac47b9f
Joris Reynes <jo.r6349@gmail.com>
2024-12-09 21:02:31 +0100

    delete folder

--- cestquoilouverture/settings.py
+++ cestquoilouverture/settings.py
@@ -74,12 +74,18 @@ WSGI_APPLICATION = 'cestquoilouverture.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': 'ironjerseys_cestquoilouverture', 
+        'USER': '<DB_USER>', 
+        'PASSWORD': '<DB_PASSWORD>', 
+        'HOST': 'postgresql-ironjerseys.alwaysdata.net', 
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
