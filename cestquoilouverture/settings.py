cbdc2eb644807cf361e7fe541744df026120da72
Joris Reynes <jo.r6349@gmail.com>
2024-12-10 21:34:46 +0100

    update settings

--- cestquoilouverture/settings.py
+++ cestquoilouverture/settings.py
@@ -130,6 +130,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Facultatif : Répertoire où Django recherchera les fichiers statiques dans vos applications
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
