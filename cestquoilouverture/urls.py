c49683e0f1d58ec8ed3e1f9e815f08645c471d7d
Joris Reynes <jo.r6349@gmail.com>
2024-12-15 07:59:48 +0100

    update urls

--- cestquoilouverture/urls.py
+++ cestquoilouverture/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from openings import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('openings/', include('openings.urls')),
+    path('', views.opening_view, name='home'),
 ]
