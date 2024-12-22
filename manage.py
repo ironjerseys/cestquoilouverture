0b1bdb9c64a8280622dcec0f6bc67cdd967ae0f6
Joris Reynes <jo.r6349@gmail.com>
2024-12-22 17:47:21 +0100

    update environnement settings

--- manage.py
+++ manage.py
@@ -6,7 +6,19 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cestquoilouverture.settings')
+    # Détermine l'environnement (par défaut : dev)
+    environment = os.getenv('DJANGO_ENV', 'dev')  # Utilise 'dev' si DJANGO_ENV n'est pas défini
+    
+    if environment == 'prod':
+        settings_module = 'cestquoilouverture.settings.prod'
+    elif environment == 'dev':
+        settings_module = 'cestquoilouverture.settings.dev'
+    else:
+        raise ValueError(f"Unknown DJANGO_ENV: {environment}")
+
+    # Définit le module de paramètres approprié
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
